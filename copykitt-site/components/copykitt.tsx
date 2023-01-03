41299faddd20416d352c2824e1ddfa2664add4f7
horizenight <kshitijroodkee1@gmail.com>
2023-01-04 01:59:42 +0530

    Css Done

--- copykitt-site/components/copykitt.tsx
+++ copykitt-site/components/copykitt.tsx
@@ -1,6 +1,9 @@
 import React from "react";
 import Form from "./form";
 import Results from "./results";
+import Image from "next/image";
+import logo1 from "../public/real.png";
+
 const CopyKitt: React.FC = () => {
   const [prompt, setPrompt] = React.useState("");
   const [snippet, setSnippet] = React.useState("");
@@ -8,16 +11,14 @@ const CopyKitt: React.FC = () => {
   const [hasResuls, setHasResults] = React.useState(false);
   const [isLoading, setIsLoading] = React.useState(false);
 
-  const CHARACTER_LIMIT :number = 32
+  const CHARACTER_LIMIT: number = 32;
 
   const onSubmit = () => {
-
-
     const ENDPOINT: string =
       "https://gr1f1bsuj4.execute-api.us-east-1.amazonaws.com/prod/generate_snippets_and_keywords";
 
     console.log("Submitting: " + prompt);
-    setIsLoading(true)
+    setIsLoading(true);
     fetch(`${ENDPOINT}?prompt=${prompt}`)
       .then((res) => res.json())
       .then(onResult);
@@ -27,7 +28,7 @@ const CopyKitt: React.FC = () => {
     setSnippet(data.snippet);
     setKeywords(data.keywords);
     setHasResults(true);
-    setIsLoading(false)
+    setIsLoading(false);
   };
   // console.log(snippet)
   // console.log(keywords)
@@ -35,7 +36,7 @@ const CopyKitt: React.FC = () => {
   const onReset = () => {
     setPrompt("");
     setHasResults(false);
-    setIsLoading(false)
+    setIsLoading(false);
   };
 
   let displayedElement = null;
@@ -51,16 +52,42 @@ const CopyKitt: React.FC = () => {
     );
   } else {
     displayedElement = (
-      <Form prompt={prompt} setPrompt={setPrompt} onSubmit={onSubmit} characterLimit={ CHARACTER_LIMIT} isLoading={isLoading} />
+      <Form
+        prompt={prompt}
+        setPrompt={setPrompt}
+        onSubmit={onSubmit}
+        characterLimit={CHARACTER_LIMIT}
+        isLoading={isLoading}
+      />
     );
   }
 
-  return (
-    <>
-      <h1>CopyKitt!</h1>
+  
+  const gradientTextStyle =
+    "text-white text-transparent bg-clip-text bg-gradient-to-r from-teal-400 to-blue-500 font-light w-fit mx-auto ";
 
-      {displayedElement}
-    </>
+
+
+  return (
+    <div className="h-screen flex">
+      <div className="max-w-md m-auto p-2">
+        <div className="bg-slate-900 p-6 rounded-md text-white">
+          <div className="text-center my-6 content-center">
+            <Image
+              src={logo1}
+              className="rounded-md  mx-auto mb-6"
+              width={72}
+              height={72}
+              alt="BrandGenerator"
+            />
+            <h1 className={gradientTextStyle+" text-3xl font-medium "}>Brand.ai</h1>
+          <div className={gradientTextStyle + "  font-thin"}>Your AI branding assistant</div>
+          </div>
+
+          {displayedElement}
+        </div>
+      </div>
+    </div>
   );
 };
 
