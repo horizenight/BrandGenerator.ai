0cf9eac7bf239872d40b33cf103647e2fd7b0258
horizenight <kshitijroodkee1@gmail.com>
2023-01-04 02:13:46 +0530

    corrected

--- copykitt-site/pages/index.tsx
+++ copykitt-site/pages/index.tsx
@@ -2,7 +2,7 @@ import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from '@next/font/google'
 import styles from '../styles/Home.module.css'
-import CopyKitt from '../Components/copykitt'
+import CopyKitt from '../components/copykitt'
 
 const inter = Inter({ subsets: ['latin'] })
 
